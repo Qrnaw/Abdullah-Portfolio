d7a3d4419c722ddccce65497c41abd1a437a0c31
Abdullah Alqarni <abdullah4alqarni@gmail.com>
2022-06-23 06:43:31 +0300

    Add files via upload
    Here I will present my journey of experiences, skills and projects that I will be working on.

---
+++ script.js
@@ -0,0 +1,46 @@
+// scrolling (navbar)
+$(document).ready(function () {
+    $(window).scroll(function () {
+        // sticky navbar on scroll script
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+
+        // scroll-up button show/hide script
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+});
+
+// Qualification -- My journey section TABS
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification-active')
+        })
+        target.classList.add('qualification-active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification-active')
+        })
+        tab.classList.add('qualification-active')
+    })
+})
+
+// Typing animution
+var Typed = new Typed(".typing", {
+    strings: ["Developer","Designer","Freelancer"],
+    typeSpeed: 150,
+    backSpeed:60,
+    loop: true
+})
